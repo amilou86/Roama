b161ddc2593f68c2890ecf42acd5c17df453fe7a
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-03-13 20:49:59 +0000

    friendlist folder

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Scroll from './components/Scroll/scroll.jsx';
 import './App.css'
-import Search from "./components/left-nav/search"
+import Search from './components/left-nav/search'
+import FriendList from './components/FriendList/friendList'
 
 function App() {
   const [count, setCount] = useState(0);
---
+++ src/components/FriendList/friendsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './friendList.css';
+
+
+
+export default function FriendList() {
+    return (
+        <div>index</div>
+    )
+}
+
