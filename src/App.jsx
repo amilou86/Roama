38d02d11fec6e9c06a8c0207226325747cefd049
Lecheeky <thadjlakehal@gmail.com>
2024-03-13 21:06:06 +0000

    adding search code and installed react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
-=======
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Search from "./components/left-nav/search"
 
+
 function App() {
   return <Search />
->>>>>>> c26a3dd0c9e4ad85c6d6d2c9bec7079311c8d1ac
 }
 
 export default App;
