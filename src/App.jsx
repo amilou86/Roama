50e1f9eac3139acb489bd64c575c11627fc4b003
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-13 20:50:06 +0000

    added general layout to app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import "./App.css";
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Scroll from './components/Scroll/scroll.jsx';
-import './App.css'
-import Search from "./components/left-nav/search"
+import { useState } from "react";
+import Scroll from "./components/Scroll/scroll.jsx";
+import "./App.css";
+import Search from "./components/left-nav/search";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
 
 function App() {
   const [count, setCount] = useState(0);
-  return <Search />
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-3">
+          <Search />
+        </div>
+        <div className="col-lg-6">
+          <Scroll />
+        </div>
+        <div className="col-lg-3">
+            posts
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
