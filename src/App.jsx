611576fb786c9c7fe28ec357c94b5e22d0b13b86
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-13 21:13:53 +0000

    added some data to scroll component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import { useState } from "react";
-import Scroll from "./components/Scroll/scroll.jsx";
+import Scroll from "./components/Scroll/Scroll.jsx";
 import "./App.css";
+
 import Search from "./components/left-nav/search";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import * as bootstrap from 'bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,9 +18,7 @@ function App() {
         <div className="col-lg-6">
           <Scroll />
         </div>
-        <div className="col-lg-3">
-            posts
-        </div>
+        <div className="col-lg-3">posts</div>
       </div>
     </div>
   );
