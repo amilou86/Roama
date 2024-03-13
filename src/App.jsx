96091740ac146ecae8b4d02b6ee7c8c5b55637e7
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-13 20:03:43 +0000

    resolved merge conflict.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
-=======
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -14,8 +7,8 @@ import './App.css'
 import Search from "./components/left-nav/search"
 
 function App() {
+  const [count, setCount] = useState(0);
   return <Search />
->>>>>>> c26a3dd0c9e4ad85c6d6d2c9bec7079311c8d1ac
 }
 
 export default App;
