570347d70a8fd686536492a67a837a58d0d34f96
ShaliniS FEBootCamper <118174874+ShaliniSunkuru@users.noreply.github.com>
2024-03-14 22:24:18 +0000

    updated events and state for login/signup pages

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,34 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import './login.css';
 
+
 export default function Login(){
+
+    //Set initial state for login form
+    const [loginData, setLoginData] = useState({
+        username: '',
+        password: '',
+    });
+    // const [errors, setErrors] = useState({});
+    
+    const handleInputChange = (event) => {
+        //Get the value and name of field whose input changed
+        let value = event.target.value;
+        const name = event.target.name;
+
+        //update state
+        setLoginData({
+            //copy of object 
+            ...loginData,
+            [name]: value,
+        });
+        // console.log(loginData);
+    }
+
+    const handleFormSubmit = (event) => {
+        //prevent default behaiviour on form submit
+        event.preventDefault();
+        console.log(loginData);
+    }
+
+
     return(
-        <div className="signPage d-flex bg-primary vh-100 justify-content-center align-items-center">
-                
-                <form className= "signForm p-3 bg-white rounded d-flex flex-column justify-content-center align-items-center" >
-                    <img className="w-50 mb-3" src="/roama-logo.png" alt="Roama" />
-                    <div className="form-group mb-3 w-100">
-                        <input type="text" className="form-control" placeholder="Username" />
-                    </div>
-                    <div className="form-group w-100">
-                        <input type="password" className="form-control" placeholder="Password" />
-                    </div>
-                    <small className="form-text mb-3">
-                        <a href="#">Forgot Password?</a>
+        <div className="signPage d-flex bg-primary vh-100 justify-content-center align-items-center">                
+            <form className= "signForm p-3 bg-white rounded d-flex flex-column justify-content-center align-items-center" >
+                <img className="w-50 mb-3" src="/roama-logo.png" alt="Roama" />
+                <div className="form-group mb-3 w-100">
+                    <input 
+                        type="text"
+                        className="form-control" 
+                        placeholder="Username"
+                        name="username"
+                        value={loginData.username}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div className="form-group w-100">
+                    <input 
+                        type="password"
+                        className="form-control" 
+                        placeholder="Password"
+                        name="password"
+                        value={loginData.password}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <small className="form-text mb-3">
+                    <a href="#">Forgot Password?</a>
+                </small>
+                <button 
+                    type="submit" 
+                    className="btn signBtn mt-3 w-100 rounded border-0"
+                    onClick={handleFormSubmit}
+                >Log in
+                </button>   
+                <hr className="mt-4 w-100"/>
+                <div className="mb-3">
+                    <small>
+                        New to Roama? 
+                        <a className="ms-2 " href="#">Sign up</a>
                     </small>
-                    <button type="submit" className="btn signBtn mt-3 w-100 rounded border-0">Log in</button>   
-                    <hr className="mt-4 w-100"/>
-
-                    <div className="mb-3">
-                        <small>
-                            New to Roama? 
-                            <a className="ms-2 " href="#">Sign up</a>
-                        </small>
-                    </div>
-                    
-
-                </form>
-
+                </div>                    
+            </form>
         </div>
     )
 }
