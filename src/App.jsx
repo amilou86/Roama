5b83b3fd4752c348d4fe104f5f0ce54ce34198ec
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-14 20:48:50 +0000

    created home component to have all components in it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,16 @@
-import Search from "./components/left-nav/search"
-import Messages from "./components/left-nav/messages.jsx";
-import Scroll from "./components/Scroll/scroll.jsx";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import * as bootstrap from 'bootstrap';
-import FriendList from "./components/FriendList/friendList.jsx";
-import Post from "./components/PostModal";
-import RightNav from "./components/right-nav/index.jsx";
-// import React, { useEffect } from 'react';
-// import { translateText } from './components/utils/api.js';
+import React from 'react';
+import Home from "./components/Home/Home"
 
-function App() {
-
+export default function App() {
   // // useEffect to trigger translation on component mount
-  // useEffect(() => {    
+  // useEffect(() => {
   //   translateText();
-  //   // empty array ensures the useEffect runs once only on component mount 
+  //   // empty array ensures the useEffect runs once only on component mount
   //   }, []);
 
   return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-lg-3">
-          <Search />
-          <Messages />
-        </div>
-        <div className="col-lg-6">
-          <Scroll />
-        </div>
-        <div className="col-lg-3">
-          <RightNav />
-          <Post />
-        </div>
-      </div>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
-
-export default App;
