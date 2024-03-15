cdbf76c45719c365f5d413076786904d71a977b4
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-15 00:35:36 +0000

    added functionality to like button.

--- src/components/Scroll/Feed.jsx
+++ src/components/Scroll/Feed.jsx
@@ -1,40 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Timestamp from "react-timestamp";
-
 import "./scroll.css";
 
 export default function Feed({ username, postImage, profilePicture, post }) {
   let currentTime = new Date();
   let postTime = currentTime;
+  const [likes, setLikes] = useState(0);
+  const handleLikes = (event) => {
+    event.preventDefault()
+    setLikes(likes + 1);
+  };
   return (
-      <article className="post">
-        <header>
-          <div className="user">
-            <div className="profile-picture">
-              <img src={profilePicture} alt={username} />
-            </div>
-            <div className="user-nickname">
-              <span>{username}</span>
-            </div>
-            <div className="user-nickname">
-              <span>
-                <Timestamp relative date={postTime} autoUpdate />
-              </span>
-            </div>
+    <article className="post">
+      <header>
+        <div className="user">
+          <div className="profile-picture">
+            <img src={profilePicture} alt={username} />
           </div>
-        </header>
-        <div className="post-image">
-          <div className="post-image-bg">
-            <img alt={postImage} src={postImage} />
+          <div className="user-nickname">
+            <span>{username}</span>
+          </div>
+          <div className="user-nickname">
+            <span>
+              <Timestamp relative date={postTime} autoUpdate />
+            </span>
           </div>
         </div>
-        <div className="post-caption">
+      </header>
+      <div className="post-image">
+        <div className="post-image-bg">
+          <img alt={postImage} src={postImage} />
+        </div>
+      </div>
+      <div className="post-caption">
         <div>
           <i className="fa-regular fa-comment"></i>
-          <i className="fa-regular fa-heart"></i>
+          <a onClick={handleLikes} href="">
+            <i className="fa-regular fa-heart"></i>
+          </a>
+          {likes} Likes
         </div>
-          {post}
-        </div>
-      </article>
+        {post}
+      </div>
+    </article>
   );
 }
