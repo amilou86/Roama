9dc88e9792a006160e95aafaf8b1f8bbd1581b63
ShaliniS FEBootCamper <118174874+ShaliniSunkuru@users.noreply.github.com>
2024-03-17 17:39:59 +0000

    styling and icons on login/signup pages

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import './login.css';
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
-
+import { FaUser }  from "react-icons/fa6";
+import { IoBag } from "react-icons/io5";
 
 export default function Login(props){
 
@@ -49,39 +50,52 @@ export default function Login(props){
         // }
     }
 
-
     return(
         <div className="signPage d-flex bg-primary vh-100 justify-content-center align-items-center">                
             <form className= "signForm p-3 bg-white rounded d-flex flex-column justify-content-center align-items-center" 
             onSubmit={handleSubmit(onSubmit)}>
                 <img className="w-50 mb-3" src="/roama-logo.png" alt="Roama" />
-                <div className="form-group mb-3 w-100">
+                <div className="input-group w-100">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text h-100" id="plane-lock">
+                            <FaUser  className="form-icon" />
+                        </span>                       
+                    </div>
                     <input 
                         type="text"
                         className="form-control" 
-                        placeholder="Username"
+                        placeholder="Username*"
                         name="username"
                         {...register("username", {
                             // required: "Please enter username"
                         })}                       
                     />
-                    {/* {errors.username &&
-                        <small className="ms-1 errorMsg">{errors.username.message}</small>} */}
                 </div>
-                <div className="form-group w-100">
+                {errors.username &&
+                     <small className="ms-1 errorMsg">{errors.username.message}</small>}
+
+                <div className="input-group mt-3 w-100">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text h-100" id="plane-lock">
+                            <IoBag className="form-icon" />
+                        </span>
+                        
+                    </div>
+                    
                     <input 
                         type="password"
                         className="form-control" 
-                        placeholder="Password"
+                        placeholder="Password*"
                         name="password"
+                        aria-describedby="plane-lock"
                         {...register("password", {
                             // required: "Please enter your password", 
                         })}
                     />
-                    {/* {errors.password &&
-                        <small className="ms-1 errorMsg">{errors.password.message}</small>} */}
                 </div>
-                <small className="form-text mb-3">
+                 {errors.password &&
+                    <small className="ms-1 errorMsg">{errors.password.message}</small>}
+                <small className="form-text mt-2 ms-auto">
                     <a href="#">Forgot Password?</a>
                 </small>
                 <button 
