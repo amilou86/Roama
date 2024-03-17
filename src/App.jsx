0572e3d2804067919a10ce3ce576714dc30d4ea3
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-17 23:47:06 +0000

    moved create new post to new page.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Home from "./components/Home/Home";
 import Login from "./components/Login";
 import Signup from './components/Login/signup';
+import Post from "./components/Middle-Section/PostPage";
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
@@ -27,6 +29,7 @@ export default function App() {
         <Route path='/' element={<Login userData={userData} setUserData={setUserData} />}  />
         <Route path='/signup' element={<Signup/>}  />
         <Route path='/home' element={<Home/>} />
+        <Route path='/newpost' element={<Post/>} />
       </Routes>
     </Router>
 
--- src/components/Middle-Section/Scroll.jsx
+++ src/components/Middle-Section/Scroll.jsx
@@ -1,23 +1,39 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Pagination from "../Pagination/Pagination";
 import "./styles.css";
 import Feed from "./Feed";
 import Post from "./Post";
 import posts from "../utils/random-posts.json";
 export default function Scroll() {
+  // fetching data with json-server
+
+  //   const [newPosts, setNewPosts] = useState([]);
+  //   useEffect(() => {
+  //     fetch("http://localhost:8000/posts")
+  //       .then((result) => {
+  //         return result.json();
+  //       })
+  //       .then((data) => {
+  //         setNewPosts(data);
+  //       });
+  //   }, []);
 
   //   pagination of posts
+
   const [currentPage, setCurrentPage] = useState(1);
-  const postsPerPage = 2;
+  const postsPerPage = 3;
 
   // get current posts
+
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   // pagination using load more anchor tag
-  //   const PAGE_SIZE = 3; // or whatever you like
+
+  //   const PAGE_SIZE = 5;
   //   const [index, setIndex] = useState(0);
   //   const [visibleData, setVisibleData] = useState([]);
   //   useEffect(() => {
@@ -29,31 +45,61 @@ export default function Scroll() {
   //     setVisibleData(newArray);
   //   }, [index]);
 
+  const navigate = useNavigate();
+  const postForm = (event) => {
+    event.preventDefault();
+    navigate("/newpost");
+  };
+
   return (
     <div id="main-section">
       <div>
-        <Post />
+        {/* <Post /> */}
+
+        {/* navigating to post in a new page */}
+
+        <input
+        id="post-prompt"
+        className="form-control"
+        type="text"
+        placeholder="Start a post"
+        onClick={postForm}
+      />
 
         {/* load more posts */}
-        {/* {visibleData.map((post, index) => ( */}
 
-        {/* pagination to load more posts */}
-        {currentPosts.map((post, index) => (
-          <Feed
-            key={index}
-            username={post.username}
-            profilePicture={post.profilePicture}
-            postImage={post.postImage}
-            post={post.post}
-            time={post.timestamp}
-          />
-        ))}
-      </div>
+        {/* {visibleData &&
+          visibleData.map((post, index) => (
+            <Feed
+              key={index}
+              username={post.username}
+              profilePicture={post.profilePicture}
+              postImage={post.postImage}
+              post={post.post}
+              time={post.timestamp}
+            />
+          ))} */}
 
-      {/* load more anchor tag */}
-      {/* <div id="load-more">
+        {/* load more anchor tag */}
+
+        {/* <div id="load-more">
         <a onClick={() => setIndex(index + 1)}> Load More </a>
       </div> */}
+
+        {/* pagination to load more posts */}
+
+        {currentPosts &&
+          currentPosts.map((post, index) => (
+            <Feed
+              key={index}
+              username={post.username}
+              profilePicture={post.profilePicture}
+              postImage={post.postImage}
+              post={post.post}
+              time={post.timestamp}
+            />
+          ))}
+      </div>
 
       <Pagination
         postsPerPage={postsPerPage}
