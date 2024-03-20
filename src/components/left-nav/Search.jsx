b91102666455153fd123637779e5797219e0deba
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-03-20 15:32:32 +0000

    created header.

--- src/components/left-nav/Search.jsx
+++ src/components/left-nav/Search.jsx
@@ -7,9 +7,6 @@ function Search(){
     return (
         <>
             <div className="left-nav">
-                <div className="logo-container">
-                    <img className="logo" src={logo}></img>
-                </div>
                 <form className="search-container">
                     <span className="search-icon"><IoSearch /></span>
                     <input placeholder="Search" className="leftnav-search" type="search"></input>
